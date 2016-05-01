f7b156c2fb5ba1da6c96678e9239e0fe565e690c
Kilik Kuo <kikuo@mozilla.com>
2016-05-01 18:45:24 +0800

    Add map for tag value to tag string, and add function logic to parse Exif Sub IFD

--- JPEGMetadataParser.py
+++ JPEGMetadataParser.py
@@ -1,5 +1,13 @@
 # exif tag definition.
-#http://www.media.mit.edu/pia/Research/deepview/exif.html
+# http://www.media.mit.edu/pia/Research/deepview/exif.html
+# ====
+# metadata structure
+# http://metalith.ru/en/help_files/structure.htm
+# ===
+# Exchangeable image file format for digital still cameras: Exif Version 2.3
+# http://www.cipa.jp/std/documents/e/DC-008-2012_E.pdf
+# ===
+
 BYTE_ALIGN_INTEL    = 0x4949
 BYTE_ALIGN_MOTOROLA = 0x4d4d
 
@@ -337,7 +345,263 @@ related to image generation by digital still cameras.
 """
 TAGID_InteroperabilityIndex           = 0x0001
 # ===========================================================
-
+{0: 'GPSVersionID',
+ 1: 'GPSLatitudeRef',
+ 2: 'GPSLatitude',
+ 3: 'GPSLongitudeRef',
+ 4: 'GPSLongitude',
+ 5: 'GPSAltitudeRef',
+ 6: 'GPSAltitude',
+ 7: 'GPSTimeStamp',
+ 8: 'GPSSatellites',
+ 9: 'GPSStatus',
+ 10: 'GPSMeasureMode',
+ 11: 'GPSDOP',
+ 12: 'GPSSpeedRef',
+ 13: 'GPSSpeed',
+ 14: 'GPSTrackRef',
+ 15: 'GPSTrack',
+ 16: 'GPSImgDirectionRef',
+ 17: 'GPSImgDirection',
+ 18: 'GPSMapDatum',
+ 19: 'GPSDestLatitudeRef',
+ 20: 'GPSDestLatitude',
+ 21: 'GPSDestLongitudeRef',
+ 22: 'GPSDestLongitude',
+ 23: 'GPSDestBearingRef',
+ 24: 'GPSDestBearing',
+ 25: 'GPSDestDistanceRef',
+ 26: 'GPSDestDistance',
+ 27: 'GPSProcessingMethod',
+ 28: 'GPSAreaInformation',
+ 29: 'GPSDateStamp',
+ 30: 'GPSDifferential',
+ 254: 'NewSubfileType',
+ 255: 'SubfileType',
+ 256: 'ImageWidth',
+ 257: 'ImageLength',
+ 258: 'BitsPerSample',
+ 259: 'Compression',
+ 262: 'PhotometricInterpretation',
+ 263: 'Threshholding',
+ 264: 'CellWidth',
+ 265: 'CellLength',
+ 266: 'FillOrder',
+ 269: 'DocumentName',
+ 270: 'ImageDescription',
+ 271: 'Make',
+ 272: 'Model',
+ 273: 'StripOffsets',
+ 274: 'Orientation',
+ 277: 'SamplesPerPixel',
+ 278: 'RowsPerStrip',
+ 279: 'StripByteCounts',
+ 280: 'MinSampleValue',
+ 281: 'MaxSampleValue',
+ 282: 'XResolution',
+ 283: 'YResolution',
+ 284: 'PlanarConfiguration',
+ 285: 'PageName',
+ 286: 'XPosition',
+ 287: 'YPosition',
+ 288: 'FreeOffsets',
+ 289: 'FreeByteCounts',
+ 290: 'GrayResponseUnit',
+ 291: 'GrayResponseCurve',
+ 292: 'T4Options',
+ 293: 'T6Options',
+ 296: 'ResolutionUnit',
+ 297: 'PageNumber',
+ 301: 'TransferFunction',
+ 305: 'Software',
+ 306: 'DateTime',
+ 315: 'Artist',
+ 316: 'HostComputer',
+ 317: 'Predictor',
+ 318: 'WhitePoint',
+ 319: 'PrimaryChromaticities',
+ 320: 'ColorMap',
+ 321: 'HalftoneHints',
+ 322: 'TileWidth',
+ 323: 'TileLength',
+ 324: 'TileOffsets',
+ 325: 'TileByteCounts',
+ 326: 'BadFaxLines',
+ 327: 'CleanFaxData',
+ 328: 'ConsecutiveBadFaxLines',
+ 330: 'SubIFDs',
+ 332: 'InkSet',
+ 333: 'InkNames',
+ 334: 'NumberOfInks',
+ 336: 'DotRange',
+ 337: 'TargetPrinter',
+ 338: 'ExtraSamples',
+ 339: 'SampleFormat',
+ 340: 'SMinSampleValue',
+ 341: 'SMaxSampleValue',
+ 342: 'TransferRange',
+ 343: 'ClipPath',
+ 344: 'XClipPathUnits',
+ 345: 'YClipPathUnits',
+ 346: 'Indexed',
+ 347: 'JPEGTables',
+ 351: 'OPIProxy',
+ 400: 'GlobalParametersIFD',
+ 401: 'ProfileType',
+ 402: 'FaxProfile',
+ 403: 'CodingMethods',
+ 404: 'VersionYear',
+ 405: 'ModeNumber',
+ 433: 'Decode',
+ 434: 'DefaultImageColor',
+ 512: 'JPEGProc',
+ 513: 'JPEGInterchangeFormat',
+ 514: 'JPEGInterchangeFormatLength',
+ 515: 'JPEGRestartInterval',
+ 517: 'JPEGLosslessPredictors',
+ 518: 'JPEGPointTransforms',
+ 519: 'JPEGQTables',
+ 520: 'JPEGDCTables',
+ 521: 'JPEGACTables',
+ 529: 'YCbCrCoefficients',
+ 530: 'YCbCrSubSampling',
+ 531: 'YCbCrPositioning',
+ 532: 'ReferenceBlackWhite',
+ 559: 'StripRowCounts',
+ 700: 'XMP',
+ 32781: 'ImageID',
+ 32932: 'WangAnnotation',
+ 33432: 'Copyright',
+ 33434: 'ExposureTime',
+ 33437: 'FNumber',
+ 33445: 'MDFileTag',
+ 33446: 'MDScalePixel',
+ 33447: 'MDColorTable',
+ 33448: 'MDLabName',
+ 33449: 'MDSampleInfo',
+ 33450: 'MDPrepDate',
+ 33451: 'MDPrepTime',
+ 33452: 'MDFileUnits',
+ 33550: 'ModelPixelScaleTag',
+ 33723: 'IPTC',
+ 33918: 'INGRPacketDataTag',
+ 33919: 'INGRFlagRegisters',
+ 33920: 'IrasBTransformationMatrix',
+ 33922: 'ModelTiepointTag',
+ 34264: 'ModelTransformationTag',
+ 34377: 'Photoshop',
+ 34665: 'ExifIFD',
+ 34675: 'ICCProfile',
+ 34732: 'ImageLayer',
+ 34735: 'GeoKeyDirectoryTag',
+ 34736: 'GeoDoubleParamsTag',
+ 34737: 'GeoAsciiParamsTag',
+ 34850: 'ExposureProgram',
+ 34852: 'SpectralSensitivity',
+ 34853: 'GPSIFD',
+ 34855: 'ISOSpeedRatings',
+ 34856: 'OECF',
+ 34908: 'HylaFAXFaxRecvParams',
+ 34909: 'HylaFAXFaxSubAddress',
+ 34910: 'HylaFAXFaxRecvTime',
+ 36864: 'ExifVersion',
+ 36867: 'DateTimeOriginal',
+ 36868: 'DateTimeDigitized',
+ 37121: 'ComponentsConfiguration',
+ 37122: 'CompressedBitsPerPixel',
+ 37377: 'ShutterSpeedValue',
+ 37378: 'ApertureValue',
+ 37379: 'BrightnessValue',
+ 37380: 'ExposureBiasValue',
+ 37381: 'MaxApertureValue',
+ 37382: 'SubjectDistance',
+ 37383: 'MeteringMode',
+ 37384: 'LightSource',
+ 37385: 'Flash',
+ 37386: 'FocalLength',
+ 37396: 'SubjectArea',
+ 37500: 'MakerNote',
+ 37510: 'UserComment',
+ 37520: 'SubsecTime',
+ 37521: 'SubsecTimeOriginal',
+ 37522: 'SubsecTimeDigitized',
+ 37724: 'ImageSourceData',
+ 40960: 'FlashpixVersion',
+ 40961: 'ColorSpace',
+ 40962: 'PixelXDimension',
+ 40963: 'PixelYDimension',
+ 40964: 'RelatedSoundFile',
+ 40965: 'InteroperabilityIFD',
+ 41483: 'FlashEnergy',
+ 41484: 'SpatialFrequencyResponse',
+ 41486: 'FocalPlaneXResolution',
+ 41487: 'FocalPlaneYResolution',
+ 41488: 'FocalPlaneResolutionUnit',
+ 41492: 'SubjectLocation',
+ 41493: 'ExposureIndex',
+ 41495: 'SensingMethod',
+ 41728: 'FileSource',
+ 41729: 'SceneType',
+ 41730: 'CFAPattern',
+ 41985: 'CustomRendered',
+ 41986: 'ExposureMode',
+ 41987: 'WhiteBalance',
+ 41988: 'DigitalZoomRatio',
+ 41989: 'FocalLengthIn35mmFilm',
+ 41990: 'SceneCaptureType',
+ 41991: 'GainControl',
+ 41992: 'Contrast',
+ 41993: 'Saturation',
+ 41994: 'Sharpness',
+ 41995: 'DeviceSettingDescription',
+ 41996: 'SubjectDistanceRange',
+ 42016: 'ImageUniqueID',
+ 42112: 'GDAL_METADATA',
+ 42113: 'GDAL_NODATA',
+ 50215: 'OceScanjobDescription',
+ 50216: 'OceApplicationSelector',
+ 50217: 'OceIdentificationNumber',
+ 50218: 'OceImageLogicCharacteristics',
+ 50706: 'DNGVersion',
+ 50707: 'DNGBackwardVersion',
+ 50708: 'UniqueCameraModel',
+ 50709: 'LocalizedCameraModel',
+ 50710: 'CFAPlaneColor',
+ 50711: 'CFALayout',
+ 50712: 'LinearizationTable',
+ 50713: 'BlackLevelRepeatDim',
+ 50714: 'BlackLevel',
+ 50715: 'BlackLevelDeltaH',
+ 50716: 'BlackLevelDeltaV',
+ 50717: 'WhiteLevel',
+ 50718: 'DefaultScale',
+ 50719: 'DefaultCropOrigin',
+ 50720: 'DefaultCropSize',
+ 50721: 'ColorMatrix1',
+ 50722: 'ColorMatrix2',
+ 50723: 'CameraCalibration1',
+ 50724: 'CameraCalibration2',
+ 50725: 'ReductionMatrix1',
+ 50726: 'ReductionMatrix2',
+ 50727: 'AnalogBalance',
+ 50728: 'AsShotNeutral',
+ 50729: 'AsShotWhiteXY',
+ 50730: 'BaselineExposure',
+ 50731: 'BaselineNoise',
+ 50732: 'BaselineSharpness',
+ 50733: 'BayerGreenSplit',
+ 50734: 'LinearResponseLimit',
+ 50735: 'CameraSerialNumber',
+ 50736: 'LensInfo',
+ 50737: 'ChromaBlurRadius',
+ 50738: 'AntiAliasStrength',
+ 50740: 'DNGPrivateData',
+ 50741: 'MakerNoteSafety',
+ 50778: 'CalibrationIlluminant1',
+ 50779: 'CalibrationIlluminant2',
+ 50780: 'BestQualityScale',
+ 50784: 'AliasLayerMetadata'}
+# ===========================================================
 lstMsgTags = []
 def log(msg, tag=None, op=None):
     if op == 'add' and tag not in lstMsgTags:
@@ -355,6 +619,13 @@ class IFDEntry:
 
     def setData(self, value):
         self.__value = value
+
+    def getTag(self):
+        return self.__tagNumber
+
+    def getValue(self):
+        assert not (len(self.__value) != 1), "Contains not exact 1 element !"
+        return self.__value[0]
 
 EXIF_TIFF_DATAFORMAT_LIST = [1,1,1,2,4,8,1,1,2,4,8,4,8]
 class JPEGMetadataParser:
@@ -473,6 +744,7 @@ class JPEGMetadataParser:
             pass
         entry.setData(data)
         log(" --- tag(%s), %s"%(hex(tag), str(data)))
+        return entry
 
     def __parseBasicIFD(self, base, start, end):
         log("Enter", "[BasicIFD]", "add")
@@ -504,10 +776,49 @@ class JPEGMetadataParser:
             else:
                 self._file.seek(posBeforeDataOffset)
 
-            self.__getDataFromFormat(tag, dataFormat, dataSize)
+            entry = self.__getDataFromFormat(tag, dataFormat, dataSize)
+            if entry.getTag() == TAGID_ExifIFD:
+                subExifOffset = entry.getValue()
+                self._file.seek(base+subExifOffset)
+                self.__parseExifSubIFD(base, start, end)
+
             self._file.seek(posAfterDataOffset)
 
         log("Leave", "[BasicIFD]", "remove")
+
+    def __parseExifSubIFD(self, base, start, end):
+        log("Enter", "[SubIFDs]", "add")
+
+        if not self._file:
+            assert False
+        entries = self.__getLen2(self._orderAPP1)
+        log("Number of entries = %d"%(entries))
+
+        for idx in xrange(entries):
+            tag = self.__getLen2(self._orderAPP1)
+            dataFormat = self.__getLen2(self._orderAPP1)
+            numOfComps = self.__getLen4(self._orderAPP1)
+            posBeforeDataOffset = self._file.tell()
+            dataOffset = self.__getLen4(self._orderAPP1)
+            posAfterDataOffset = self._file.tell()
+
+            if 0 == dataFormat or dataFormat >= len(EXIF_TIFF_DATAFORMAT_LIST):
+                assert False, "dataformat incorrect = %d"%(dataFormat)
+                continue
+            bytesPerComp = EXIF_TIFF_DATAFORMAT_LIST[dataFormat]
+            dataSize = bytesPerComp * numOfComps
+            if dataSize > 4:
+                targetOffset = base + dataOffset
+                if targetOffset <= start or targetOffset >= end:
+                    continue
+                else:
+                    self._file.seek(targetOffset)
+            else:
+                self._file.seek(posBeforeDataOffset)
+            entry = self.__getDataFromFormat(tag, dataFormat, dataSize)
+            self._file.seek(posAfterDataOffset)
+
+        log("Leave", "[SubIFDs]", "remove")
 
     def __parseAPP1(self, base, start, end):
         if not self._file:
@@ -525,10 +836,13 @@ class JPEGMetadataParser:
         if check != 0x002a:
             assert False
 
-        offsetToIFD = self.__getLen4(self._orderAPP1)
-        log("offsetToIFD = %s"%(hex(offsetToIFD)))
-        self._file.seek(base+offsetToIFD)
-        self.__parseBasicIFD(base, start, end)
+        offsetToNextIFD = self.__getLen4(self._orderAPP1)
+        log("offsetToNextIFD = %s"%(hex(offsetToNextIFD)))
+        while offsetToNextIFD:
+            self._file.seek(base+offsetToNextIFD)
+            self.__parseBasicIFD(base, start, end)
+            offsetToNextIFD = self.__getLen4(self._orderAPP1)
+            log("offsetToNextIFD = %s"%(hex(offsetToNextIFD)))
 
     def __parseXMP(self):
         if not self._file:
@@ -581,6 +895,7 @@ class JPEGMetadataParser:
 
 import os
 fPath = "./images/Sample.JPG"
+#fPath = "./images/brownie.jpg"
 fullPath = os.path.abspath(fPath)
 
 jpgParser = JPEGMetadataParser()
