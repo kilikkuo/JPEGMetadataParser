28e8a5f0a95c7236d3a8f1a4768a58606bbc622b
Kilik Kuo <kikuo@mozilla.com>
2016-06-20 23:51:02 +0800

    Try using a universal method to hide the object creation of different signature & type - 1 (Will hide the parsing knowledge in each object in the future)

--- ICCProfileParser.py
+++ ICCProfileParser.py
@@ -3,6 +3,7 @@ from misc import log
 from FileOPs import nowAt, seekTo, getChar, getCharToOrd, getBytes2, getBytes4,\
                     getBytes8, BYTE_ALIGN_INTEL, BYTE_ALIGN_MOTOROLA
 import pprint
+import struct
 
 H_CMM_TYPE                  = "CMMType"
 H_VERSION                   = "Version"
@@ -92,14 +93,67 @@ dicRenderingIntent2Desc = {
 3 : "Absolute Colorimetric",
 }
 
+dicIlluminantType2Desc = {
+hex(0)  :   "unknown",
+hex(1)  :   "D50",
+hex(2)  :   "D65",
+hex(3)  :   "D93",
+hex(4)  :   "F2",
+hex(5)  :   "D55",
+hex(6)  :   "A",
+hex(7)  :   "Equi-Power (E)",
+hex(8)  :   "F8",
+}
+
+dicTechType2Desc = {
+'fscn'  :   'Film Scanner',
+'dcam'  :   'Digital Camera',
+'rscn'  :   'Reflective Scanner',
+'ijet'  :   'Ink Jet Printer',
+'twax'  :   'Thermal Wax Printer',
+'epho'  :   'Electrophotographic Printer',
+'esta'  :   'Electrostatic Printer',
+'dsub'  :   'Dye Sublimation Printer',
+'rpho'  :   'Photographic Paper Printer',
+'fprn'  :   'Film Writer',
+'vidm'  :   'Video Monitor',
+'vidc'  :   'Video Camera',
+'pjtv'  :   'Projection Television',
+'CRT '  :   'Cathode Ray Tube Display',
+'PMD '  :   'Passive Matrix Display',
+'AMD '  :   'Active Matrix Display',
+'KPCD'  :   'Photo CD',
+'imgs'  :   'PhotoImageSetter',
+'grav'  :   'Gravure',
+'offs'  :   'Offset Lithography',
+'silk'  :   'Silkscreen',
+'flex'  :   'Flexography',
+}
+
+dicStdObserver2Desc = {
+hex(0)  :   'unknown',
+hex(1)  :   '1931 2 degree Observer',
+hex(2)  :   '1964 10 degree Observer',
+}
+
+dicGeometry2Desc = {
+hex(0)  :   'unknown',
+hex(1)  :   '0/45 or 45/0',
+hex(2)  :   '0/d or d/0',
+}
+
 class Type(object):
-    def __init__(self):
+    def __init__(self, sig=None):
+        assert sig != None, "input sig should be something !!"
+        self._sig = sig
         pass
 
 class Curve(Type):
     descriptor = "curv"
-    def __init__(self):
-        Type.__init__(self)
+    def __init__(self, sig, exp=None, table=[]):
+        Type.__init__(self, sig)
+        self._exp = exp
+        self._lookUp = table
 
 class Data(Type):
     descriptor = "data"
@@ -123,8 +177,8 @@ class Lut8(Type):
 
 class Measurement(Type):
     descriptor = "meas"
-    def __init__(self):
-        Type.__init__(self)
+    def __init__(self, sig, stdObserver, tristimulus, geometry, flare, stdIlluminant):
+        Type.__init__(self, sig)
 
 class NamedColor(Type):
     descriptor = "ncol"
@@ -153,8 +207,9 @@ class Screen(Type):
 
 class Signature(Type):
     descriptor = "sig "
-    def __init__(self):
-        Type.__init__(self)
+    def __init__(self, sig, content):
+        Type.__init__(self, sig)
+        self._techContent = content
 
 class TextDescription(Type):
     descriptor = "desc"
@@ -163,8 +218,9 @@ class TextDescription(Type):
 
 class Text(Type):
     descriptor = "text"
-    def __init__(self):
-        Type.__init__(self)
+    def __init__(self, sig, content):
+        Type.__init__(self, sig)
+        self._content = content
 
 class U16Fixed16Array(Type):
     descriptor = "uf32"
@@ -198,19 +254,167 @@ class UInt8Array(Type):
 
 class ViewingConditions(Type):
     descriptor = "view"
-    def __init__(self):
-        Type.__init__(self)
+    def __init__(self, sig, illuminantXYZ, surroundXYZ, illuinantType):
+        Type.__init__(self, sig)
+        self._absXYZIlluminant = illuminantXYZ
+        self._absXYZSurround = surroundXYZ
+        self._illuminantType = illuinantType
 
 class XYZ(Type):
     descriptor = "XYZ "
-    def __init__(self):
-        Type.__init__(self)
+    def __init__(self, sig, X, Y, Z):
+        Type.__init__(self, sig)
+        self._XYZ = (X, Y, Z)
 
+def GetXYZHelper(_fd):
+    assert _fd, "_fd should not be null !"
+    intX, intY, intZ = getBytes4(_fd), getBytes4(_fd), getBytes4(_fd)
+    if intX != 0 and intY != 0 and intZ != 0:
+        X = struct.unpack('f', struct.pack('i', intX))
+        Y = struct.unpack('f', struct.pack('i', intY))
+        Z = struct.unpack('f', struct.pack('i', intZ))
+        CIEXYZ_X = X[0] / Y[0]
+        CIEXYZ_Y = Y[0] / Y[0]
+        CIEXYZ_Z = Z[0] / Y[0]
+        return CIEXYZ_X, CIEXYZ_Y, CIEXYZ_Z
+    return 0, 0, 0
+
+def GetSigObject(sig, type, _fd, size):
+    # _fd is already seeked to starting point of data
+    # 4bytes type(description) is included in size
+    sigDescObj = None
+    if sig == "A2B0":
+        pass
+    elif sig == "A2B1":
+        pass
+    elif sig == "A2B1":
+        pass
+    elif sig == "A2B0":
+        pass
+    elif sig == "A2B1":
+        pass
+    elif sig == "A2B2":
+        pass
+    elif sig in ["bXYZ", "gXYZ", "rXYZ", "bkpt", "wtpt"]:
+        reserved = getBytes4(_fd)
+        assert reserved == 0
+        assert size == 20
+        X, Y, Z = GetXYZHelper(_fd)
+        log(" XYZ = (%f, %f, %f)"%(X, Y, Z))
+        sigDescObj = XYZ(sig, X, Y, Z)
+    elif sig == "B2A0":
+        pass
+    elif sig == "B2A1":
+        pass
+    elif sig == "B2A2":
+        pass
+    elif sig == "calt":
+        pass
+    elif sig == "targ":
+        pass
+    elif sig == "cprt":
+        content = ''.join(getChar(_fd) for _ in xrange(size-4))
+        log(" cpry content = %s"%(content))
+        sigDescObj = Text(sig, content)
+        pass
+    elif sig == "dmnd":
+        pass
+    elif sig == "dmdd":
+        pass
+    elif sig == "gamt":
+        pass
+    elif sig == "kTRC":
+        pass
+    elif sig == "lumi":
+        pass
+    elif sig == "meas":
+        reserved = getBytes4(_fd)
+        assert reserved == 0
+        assert size == 36
+        obs = dicStdObserver2Desc.get(hex(getBytes4(_fd)), dicStdObserver2Desc[hex(0)])
+        tristimulusXYZ = GetXYZHelper(_fd)
+        geo = dicGeometry2Desc.get(hex(getBytes4(_fd)), dicGeometry2Desc[hex(0)])
+        flareInt, flareFractional = getBytes2(_fd), getBytes2(_fd)
+        flare = flareInt + float(flareFractional) / 65536
+        illuminantType = dicIlluminantType2Desc.get(hex(getBytes4(_fd)), dicIlluminantType2Desc[hex(0)])
+        log(" obs(%s) / triXYZ(%s) / geo(%s) / flare(%f) / illu(%s)"%(obs, str(tristimulusXYZ),\
+        geo, flare, illuminantType))
+        sigDescObj = Measurement(sig, obs, tristimulusXYZ, geo, flare, illuminantType)
+    elif sig == "ncol":
+        pass
+    elif sig == "ncl2":
+        pass
+    elif sig == "pre0":
+        pass
+    elif sig == "pre1":
+        pass
+    elif sig == "pre2":
+        pass
+    elif sig == "desc":
+        pass
+    elif sig == "pseq":
+        pass
+    elif sig == "psd0":
+        pass
+    elif sig == "psd1":
+        pass
+    elif sig == "psd2":
+        pass
+    elif sig == "psd3":
+        pass
+    elif sig == "ps2s":
+        pass
+    elif sig == "ps2i":
+        pass
+    elif sig in ["rTRC", "gTRC", "bTRC"]:
+        reserved = getBytes4(_fd)
+        assert reserved == 0
+        count = getBytes4(_fd)
+
+        exp = None
+        tblLookUp = []
+        if count in [0, 1]:
+            first = 1.0 if count == 0 else getCharToOrd(_fd)
+            second = 0.0 if count == 0 else getCharToOrd(_fd)
+            exp = first + float(second/256.0)
+            log(" count = %d / exp(%f)"%(count, exp))
+        else:
+            for _ in xrange(count):
+                first, second = getCharToOrd(_fd), getCharToOrd(_fd)
+                v = first + float(second/256.0)
+                tblLookUp.append(v)
+            log(" count = %d "%(count))
+        sigDescObj = Curve(sig, exp, tblLookUp)
+    elif sig == "scrd":
+        pass
+    elif sig == "scrn":
+        pass
+    elif sig == "tech":
+        content = ''.join(getChar(_fd) for _ in xrange(size-4)).strip('\0x00')
+        log(" tech content = %s "%(dicTechType2Desc.get(content, 'None')))
+        sigDescObj = Signature(sig, content)
+        pass
+    elif sig == "bfd ":
+        pass
+    elif sig == "vued":
+        pass
+    elif sig == "view":
+        reserved = getBytes4(_fd)
+        assert reserved == 0
+        illuminantXYZ = GetXYZHelper(_fd)
+        surroundXYZ = GetXYZHelper(_fd)
+        log(" illXYZ = (%f, %f, %f)"%(illuminantXYZ))
+        log(" surXYZ = (%f, %f, %f)"%(surroundXYZ))
+        illuminantType = dicIlluminantType2Desc.get(hex(getBytes4(_fd)), dicIlluminantType2Desc[hex(0)])
+        sigDescObj = ViewingConditions(sig, illuminantXYZ, surroundXYZ, illuminantType)
+        log(" illuminantType = %s"%illuminantType)
+
+    return sigDescObj
 
 class ICCProfileParser(object):
     def __init__(self, fd):
         self.__dicHeaderInfo = {}
-        self.__dicTagInfo = {}
+        self.__dicSig2TagInfo = {}
         self._fdWRef = weakref.ref(fd)
 
     def parse(self):
@@ -274,7 +478,6 @@ class ICCProfileParser(object):
             self.__dicHeaderInfo[H_RENDERING_INTENT] = dicRenderingIntent2Desc.get(renderingIntent, "Not found")
 
             intX, intY, intZ = getBytes4(self._fd), getBytes4(self._fd), getBytes4(self._fd)
-            import struct
             X = struct.unpack('f', struct.pack('i', intX))
             Y = struct.unpack('f', struct.pack('i', intY))
             Z = struct.unpack('f', struct.pack('i', intZ))
@@ -300,40 +503,12 @@ class ICCProfileParser(object):
                 sig = ''.join(getChar(self._fd) for _ in xrange(4))
                 offset = getBytes4(self._fd)
                 size = getBytes4(self._fd)
-                log("Tag sig(%s) / offset(%d) / size(%d)"%(sig, offset, size))
                 seekTo(self._fd, basePos+offset)
                 typeDesc = ''.join(getChar(self._fd) for _ in xrange(4))
-                if typeDesc == "curv":
-                    reserved = getBytes4(self._fd)
-                    assert reserved == 0
-                    count = getBytes4(self._fd)
-                    log(" count = %d"%(count))
-                    if count == 0:
-                        exp = 1.0
-                    else:
-                        first, second = getCharToOrd(self._fd), getCharToOrd(self._fd)
-                        exp = first + float(second/256.0)
-                    log(" exp = %f"%(exp))
-                elif typeDesc == "XYZ ":
-                    reserved = getBytes4(self._fd)
-                    assert reserved == 0
-                    assert size == 20
-                    intX, intY, intZ = getBytes4(self._fd), getBytes4(self._fd), getBytes4(self._fd)
-                    if intX != 0 and intY != 0 and intZ != 0:
-                        import struct
-                        X = struct.unpack('f', struct.pack('i', intX))
-                        Y = struct.unpack('f', struct.pack('i', intY))
-                        Z = struct.unpack('f', struct.pack('i', intZ))
-                        CIEXYZ_X = X[0] / Y[0]
-                        CIEXYZ_Y = Y[0] / Y[0]
-                        CIEXYZ_Z = Z[0] / Y[0]
-                        log("CIEXYZ = (%f, %f, %f)"%(CIEXYZ_X, CIEXYZ_Y, CIEXYZ_Z))
-                    else:
-                        log("CIEXYZ = (%f, %f, %f)"%(0, 0, 0))
-                elif typeDesc == "text":
-                    text = ''.join(getChar(self._fd) for _ in xrange(size-4))
-                    log(" txt = %s"%(text))
-                elif typeDesc == "desc":
+                log("Tag sig(%s) / type(%s) / offset(%d) / size(%d)"%(sig, typeDesc, offset, size))
+
+                sigDescObj = None
+                if typeDesc == "desc":
                     reserved = getBytes4(self._fd)
                     assert reserved == 0
                     asciiCount = getBytes4(self._fd)
@@ -352,13 +527,16 @@ class ICCProfileParser(object):
                     if scriptCode != 0 and scriptCount != 0:
                         localMacintoshDesc = getChar(self._fd, scriptCount)
                         log(" localMacintoshDesc = %s"%(localMacintoshDesc))
-                    pass
+                else:
+                    sigDescObj = GetSigObject(sig, typeDesc, self._fd, size)
+
+                assert sig not in self.__dicSig2TagInfo, "Check this file, two same sig !"
+                self.__dicSig2TagInfo[sig] = sigDescObj
                 seekTo(self._fd, tagStartPos+12)
 
             log("Leave", "[ICCProfileTagTable]", "remove")
+            pprint.pprint(self.__dicSig2TagInfo)
             pass
-            #tagCount = getBytes4(self._fd)
-            #print "Tag count = %d"%(tagCount)
 
         parseHeader()
         parseTagTable()
